b930912783499c9e92953bb2a58367ae294d8075
Yurii <ykurleev88@gmail.com>
2020-11-29 21:49:56 +0200

    fixed comment

--- routers/jwt.js
+++ routers/jwt.js
@@ -7,7 +7,7 @@ const router = express.Router({ mergeParams: true });
 router.post('/login', JWTController.login)
 router.post('/refresh_tokens', JWTController.refresh)
 router.post('/check_access', JWTController.check)
-// events fallback error handler
+// fallback error handler
 router.use((err, req, res, next) => {
   logger.error(err);
   res.status(500).send('Unable to proceed JWT request');
